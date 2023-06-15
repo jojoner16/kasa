a7004d2405c6d253e78410f344eb8e57ac4b02f8
jojoner16 <laybats.jonathan@orange.fr>
2023-06-15 17:22:19 +0200

    amelioration de la disparition du footer quand une mauvaise url est entrée

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -3,23 +3,24 @@ import { useLocation } from 'react-router-dom';
 import FooterLogo from '../../assets/logo_footer.png';
 import '../../styles/footer.css';
 
-function Footer({ hideFooter }) {
+function Footer() {
   const location = useLocation();
 
-  // Vérifie si vous êtes sur la page d'erreur
-  const isOnErrorPage = location.pathname === '/error';
+  // Vérifie si vous êtes sur la page d'accueil ou à propos
+  const isHomePage =
+    location.pathname === '/' || location.pathname === '/accueil';
+  const isAboutPage = location.pathname === '/about';
 
   // Si vous êtes sur la page d'erreur, n'affiche pas le footer
-  if (isOnErrorPage || hideFooter) {
-    return null;
+  if (isHomePage || isAboutPage) {
+    return (
+      <footer className="footerContainer">
+        <img className="footerLogo" src={FooterLogo} alt="logo kasa" />
+        <p className="footerCopyright">© 2020 Kasa. All rights reserved</p>
+      </footer>
+    );
   }
-
-  return (
-    <footer className="footerContainer">
-      <img className="footerLogo" src={FooterLogo} alt="logo kasa" />
-      <p className="footerCopyright">© 2020 Kasa. All rights reserved</p>
-    </footer>
-  );
+  return null;
 }
 
 export default Footer;
