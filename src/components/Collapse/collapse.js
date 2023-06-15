5b6df9ebb16d4a60eefaab3f125f77f4654ac40a
jojoner16 <laybats.jonathan@orange.fr>
2023-06-15 13:22:42 +0200

    création du composant 'collapse' insertion dans la page 'à propos' et mise en forme

---
+++ src/components/Collapse/collapse.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import '../../styles/collapses.css';
+
+function Collapse({ title, content }) {
+  //declaration du state en utilisant le Hook useState()
+  const [isOpen, setIsOpen] = useState(false); // je definie le state du toggle (et false par défaut)
+
+  //fonction pour gérer l'afichage du contenu des collapses
+  const display = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    // affiche le collapse replié par défaut et l'ouvre au clic puis le referme au clic en faisant disparaitre le texte et le style
+    <div className="dropdownContainer">
+      <div className="dropdownTitle">
+        <h2>{title}</h2>
+        <p className="chevron" onClick={display}>
+          {isOpen ? (
+            <FontAwesomeIcon icon={faChevronUp} />
+          ) : (
+            <FontAwesomeIcon icon={faChevronDown} />
+          )}
+        </p>
+      </div>
+      {/* Si le collapse est à TRUE alors il affichera la description */}
+      <div className="dropdownContent">
+        {isOpen && <p className="content">{content}</p>}
+      </div>
+    </div>
+  );
+}
+
+export default Collapse;
--- src/pages/About/about.js
+++ src/pages/About/about.js
@@ -1,10 +1,31 @@
+import Banner from '../../components/Banner/banner';
+import AboutBannerImg from '../../assets/BannerPropos.png';
+import Collapse from '../../components/Collapse/collapse';
+import collapsesData from '../../data/collapses.json';
 import '../../styles/index.css';
 
 function About() {
   return (
-    <div className="about">
-      <h1>A Propos</h1>
-    </div>
+    <main>
+      <div className="about">
+        <Banner image={AboutBannerImg} alt="image bannière à propos" />
+
+        <div className="collapse">
+          <div className="dropdown">
+            {collapsesData.map((collapses) => {
+              return (
+                <div key={collapses.id}>
+                  <Collapse
+                    title={collapses.title}
+                    content={collapses.content}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
