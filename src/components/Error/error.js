db8ac89fcbe824679b0da073a2ffb7b5e46d3153
jojoner16 <laybats.jonathan@orange.fr>
2023-06-15 15:09:59 +0200

    création de la page erreur 404

---
+++ src/components/Error/error.js
@@ -0,0 +1,15 @@
+import { NavLink } from 'react-router-dom';
+
+function ErrorPage() {
+  return (
+    <div className="errorContainer">
+      <p className="errorNumber">404</p>
+      <p className="errorText">Oups! La page que vous demandez n'existe pas.</p>
+      <NavLink to="/" className="errorLink">
+        Retourner sur la page d'accueil
+      </NavLink>
+    </div>
+  );
+}
+
+export default ErrorPage;
