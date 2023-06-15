92bcf1a9dd6bece84b7ac7087237ca0cfaf9d1e4
jojoner16 <laybats.jonathan@orange.fr>
2023-06-15 15:39:29 +0200

    suppression du soulignage du texte de la navigation quand la page erreur 404 est affichée

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -5,26 +5,38 @@ import '../../styles/header.css';
 
 function Header() {
   const location = useLocation();
-  const [isHome, setIsHome] = useState(false);
+  // const isHomePage = location.pathname === '/'; // Vérifie si la page actuelle est la page d'accueil
+  const isAboutPage = location.pathname === '/about'; // Vérifie si la page actuelle est la page "À Propos"
+  const isErrorPage = location.pathname === '/error'; // Vérifie si la page actuelle est la page d'erreur
+  const [isHomePage, setIsHomePage] = useState(false);
 
   useEffect(() => {
     if (location.pathname === '/about') {
-      setIsHome(false);
+      setIsHomePage(false);
     } else {
-      setIsHome(true);
+      setIsHomePage(true);
     }
   }, [location]);
+
   return (
     <nav className="navContainer">
       <Link to="/">
         <img className="headerLogo" src={HomeLogo} alt="logo kasa" />
       </Link>
       <nav className="headerNav">
-        <NavLink to="/" className={`navLink ${isHome && 'underline'}`}>
+        <NavLink
+          to="/"
+          className={`navLink ${isHomePage && !isErrorPage ? 'underline' : ''}`}
+        >
           Accueil
         </NavLink>
-        <NavLink to="/about" className={`navLink ${!isHome && 'underline'}`}>
-          A Propos
+        <NavLink
+          to="/about"
+          className={`navLink ${
+            isAboutPage && !isErrorPage ? 'underline' : ''
+          }`}
+        >
+          À Propos
         </NavLink>
       </nav>
     </nav>
