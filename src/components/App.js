20a0d0b62cc8f085a50c48eac4eb38fa16c8b8de
jojoner16 <laybats.jonathan@orange.fr>
2023-06-23 10:02:48 +0200

    ajout de la route pour la page de chaque location

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Home from '../pages/Home/home';
 import Header from './Header/header';
 import Footer from './Footer/footer';
 import ErrorPage from './Error/error';
+import RentalSheet from '../pages/Locations/rentalSheet';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" element={<Navigate replace to="/accueil" />} />
         <Route path="/accueil" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/location/:id" element={<RentalSheet />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Footer />
