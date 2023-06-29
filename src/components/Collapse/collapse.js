9c7932ecdbe45807e81f949dfcb169a622dfd2fd
jojoner16 <laybats.jonathan@orange.fr>
2023-06-29 15:11:50 +0200

    ajout petite animation sur les tags et le chevrons

--- src/components/Collapse/collapse.js
+++ src/components/Collapse/collapse.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames';
 import '../../styles/Components/collapses.css';
 
 function Collapse({ title, content }) {
@@ -8,26 +9,33 @@ function Collapse({ title, content }) {
   const [isOpen, setIsOpen] = useState(false); // je definie le state du toggle (et false par défaut)
 
   //fonction pour gérer l'afichage du contenu des collapses
-  const display = () => {
+  const toggleCollapse = () => {
     setIsOpen(!isOpen);
   };
+
+  const chevronClasses = classNames('chevron', {
+    'chevron-rotate': isOpen,
+  });
+
+  const dropdownContentClasses = classNames('dropdownContent', {
+    'dropdownContent-open': isOpen,
+  });
 
   return (
     // affiche le collapse replié par défaut et l'ouvre au clic puis le referme au clic en faisant disparaitre le texte et le style
     <div className="dropdownContainer">
-      <div className="dropdownTitle" onClick={display}>
+      <div className="dropdownTitle" onClick={toggleCollapse}>
         <h2>{title}</h2>
-        <p className="chevron">
-          {isOpen ? (
-            <FontAwesomeIcon icon={faChevronUp} />
-          ) : (
-            <FontAwesomeIcon icon={faChevronDown} />
-          )}
+        <p className={chevronClasses}>
+          <FontAwesomeIcon
+            icon={isOpen ? faChevronUp : faChevronDown}
+            className="chevron"
+          />
         </p>
       </div>
       {/* Si le collapse est à TRUE alors il affichera la description */}
-      <div className="dropdownContent">
-        {isOpen && <p className="content">{content}</p>}
+      <div className={dropdownContentClasses}>
+        <p className="content">{content}</p>
       </div>
     </div>
   );
