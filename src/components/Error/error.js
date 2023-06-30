46998f425356d40ae2bc13922bfca4791f113d30
jojoner16 <laybats.jonathan@orange.fr>
2023-06-30 15:24:20 +0200

    mise en forme version mobile pour la page d'erreur 404

--- src/components/Error/error.js
+++ src/components/Error/error.js
@@ -4,15 +4,19 @@ import { NavLink } from 'react-router-dom';
 import '../../styles/Components/error.css';
 
 function ErrorPage() {
+  const isMobile = window.innerWidth <= 767; // Vérifie si l'écran est de taille mobile
+
+  const errorMessage = isMobile
+    ? "Oups! La page que \n vous demandez n'existe pas." // Ajoute un saut de ligne après le mot "que" dans la version mobile
+    : "Oups! La page que vous demandez n'existe pas.";
+
   return (
     <div className="errorContainer">
       <p className="errorNumber">404</p>
-      <p className="errorText">Oups! La page que vous demandez n'existe pas.</p>
+      <p className="errorText">{errorMessage}</p>
       <NavLink to="/" className="errorLink">
         Retourner sur la page d'accueil
       </NavLink>
-      {/* <Footer hideFooter={true} />{' '} */}
-      {/* Ajoutez la prop hideFooter avec la valeur true */}
     </div>
   );
 }
